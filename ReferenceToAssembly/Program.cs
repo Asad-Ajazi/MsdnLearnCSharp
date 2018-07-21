d34f473da306ca9ca0f2ee7db9f4a4afea03e104
Asad Ajazi <AsadAjazi.work@gmail.com>
2018-07-21 09:55:00 +0100

    Added a class library to help scrape web pages

---
+++ ReferenceToAssembly/Program.cs
@@ -0,0 +1,25 @@
+﻿using ScrapeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceToAssembly
+{
+    class Program
+    {
+        //to access parts of the .net framework class library.
+        //1. right click references in the solution explorer and add reference.
+        //2. Nuget
+        //-----
+        //Make our own class library to reference, here we scraped a web page and wrote it to a file.
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string result = myScrape.ScrapeWebpage("http://www.msdn.microsoft.com", @"C:\Users\asus\Desktop\Programming\CSharp\learncs\ReferenceToAssembly\WriteText.txt");
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ScrapeLibrary/Scrape.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeLibrary
+{
+    //works with the scrape client in 'ReferenceToAssemplies'
+    //filePath = @"C:\Users\asus\Desktop\Programming\CSharp\learncs\ReferenceToAssembly\WriteText.txt";
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebpage(url);
+        }
+
+        public string ScrapeWebpage(string url, string filePath)
+        {
+            string reply = GetWebpage(url);            
+            File.WriteAllText(filePath, reply);
+
+            return reply;
+        }
+        //Private method to encapsulate
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString(url);
+            reply += "IN THE END, IT DOESN'T EVEN MATTER";
+            return reply;
+        }
+
+    }
+}
